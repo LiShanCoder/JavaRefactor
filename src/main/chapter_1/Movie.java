7ba1f56a05efd4faeb1b2cb1d90b2764a08f49ae
LiShanCoder <827138050@qq.com>
2018-07-17 16:54:24 +0800

    重构：运用多态取代与价格相关的条件逻辑
    1.switch的case语句中，最好只使用自己对象的属性
    2.把容易变动的逻辑、属性，封装在自己对象中处理

--- src/main/chapter_1/Movie.java
+++ src/main/chapter_1/Movie.java
@@ -26,4 +26,35 @@ public class Movie {
 		this._priceCode = _priceCode;
 	}
 
+	/* 参数的选择上：选择[租期长度]作为参数，而不使用[影片类型]作为参数？
+	 * 		[租期长度]与Movie.class变动关系不大，置于Movie.class外部，以参数形式传入
+	 * 		[影片类型]与Movie.class关系密切，应置于Movie.class内部，与[影片类型]相关的操作都应该放在Movie.class中
+	 */
+	double getCharge(int daysRented) {
+		double result = 0;
+		switch(getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if(daysRented>2)
+				result += (daysRented-2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if(daysRented>3)
+				result += (daysRented-3) * 1.5;
+			break;
+		}
+		return result;
+	}
+	
+	int getFrequentRenterPoints(int daysRented) {
+		if((getPriceCode()==Movie.NEW_RELEASE) && daysRented>1)
+			return 2;
+		else
+			return 1;
+	}
+	
 }
--- src/main/chapter_1/Rental.java
+++ src/main/chapter_1/Rental.java
@@ -18,30 +18,14 @@ public class Rental {
 		return _daysRented;
 	}
 
+	/* 最好不要在 另一个对象的属性基础上，使用switch语句。如果不得不使用，应该使用自己对象的数据。
+	 * 这暗示，getCharge()应该移动到Movie.class中
+	 */
 	double getCharge() {
-		double result = 0;
-		switch(getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if(getDaysRented()>2)
-				result += (getDaysRented()-2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if(getDaysRented()>3)
-				result += (getDaysRented()-3) * 1.5;
-			break;
-		}
-		return result;
+		return _movie.getCharge(_daysRented);
 	}
 	
 	int getFrequentRenterPoints() {
-		if((getMovie().getPriceCode()==Movie.NEW_RELEASE) && getDaysRented()>1)
-			return 2;
-		else
-			return 1;
+		return _movie.getFrequentRenterPoints(_daysRented);
 	}
 }
